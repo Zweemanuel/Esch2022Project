5149a3eb48dd8de283cc70076254dc75c104f199
NRoeser <nickroeser@hotmail.de>
2021-05-24 20:47:14 +0200

    Added Soundeffects

---
+++ Assets/Scripts/ButtonSoundEffectScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSoundEffectScript : MonoBehaviour
+{
+    private static ButtonSoundEffectScript instance;
+    
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+}
