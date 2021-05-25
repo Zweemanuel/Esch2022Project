cc44b80894e8562332d1686658e210d80ed96874
NRoeser <nickroeser@hotmail.de>
2021-05-25 13:33:50 +0200

    Did a first setting for the options scene

---
+++ Assets/Scripts/ToggleAudio.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleAudio : MonoBehaviour
+{
+    private bool toggle = true;
+    public void ChangeTextOnClick(Text textfield)
+    {
+        if (textfield.text == "On" )
+        {
+            textfield.text = "Off";
+        } else if (textfield.text == "Off" )
+        {
+            textfield.text = "On";
+        }
+    }
+
+    public void toggleSound()
+    {
+        toggle = !toggle;
+        if (toggle)
+        {
+            AudioListener.volume = 1f;
+        } else
+        {
+            AudioListener.volume = 0f;
+        }
+    }
+}
